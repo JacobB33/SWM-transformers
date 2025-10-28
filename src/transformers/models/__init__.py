eb0451adad2e0f6efdd71d5bf406a70f57bfcada
Jacob Berg <hockeyviolin@gmail.com>
2025-10-28 13:25:10 -0700

    merge finish

--- src/transformers/models/__init__.py
+++ src/transformers/models/__init__.py
@@ -257,6 +257,7 @@ if TYPE_CHECKING:
     from .owlv2 import *
     from .owlvit import *
     from .paligemma import *
+    from .paligemma_wm import *
     from .parakeet import *
     from .patchtsmixer import *
     from .patchtst import *
