1460237e51e0f7667088dcf9a4eda056008d1c4b
Jacob Berg <jacob33@tillicum-login02.hyak.local>
2026-04-23 12:50:49 -0700

    updates to the action processing code

--- src/transformers/models/paligemma_wm/action_processor.py
+++ src/transformers/models/paligemma_wm/action_processor.py
@@ -5,40 +5,51 @@ from ...processing_utils import (
 import torch
 import numpy as np
 import typing as tp
+from torch.nn.utils.rnn import pad_sequence
+
 class PaliGemmaWMActionProcessor(ProcessorMixin):
     attributes = []
-    
+
     def __init__(
             self,
             action_seq_length: int,
             ):
-        self.action_seq_length = action_seq_length    
+        self.action_seq_length = action_seq_length
         super().__init__()
-        
-    def __call__(self, inputs: tp.Union[List[List[torch.Tensor]], torch.Tensor, np.ndarray]):
+
+    def __call__(self, inputs: tp.Union[List[torch.Tensor], torch.Tensor, np.ndarray]):
         # flatten the actions and return the list of lengths
         # Check if it is a numpy or tensor
         if isinstance(inputs, np.ndarray):
             inputs = torch.from_numpy(inputs)
-        
+
         if isinstance(inputs, torch.Tensor):
             assert len(inputs.shape) == 3, "If you pass a tensor, the shape is expected to be B, num_action, act_dim"
             lengths = [inputs.shape[1] for _ in range(len(inputs))]
             return inputs, lengths
-            
-        
+
+
         lengths = [len(input) for input in inputs]
         if sum(lengths) == 0:
             # we know that there is no actions, and it is a list of empty actions
             return None, lengths
-        # TODO optimize this? 
-        max_length = max(lengths)
-        # pad with infinite actions
-        inputs = [input + [torch.tensor([float('inf'), float('inf')]) for _ in range(max_length - len(input))] for input in inputs]
-        
-        # flattened_action = [action if isinstance(action, torch.Tensor) else torch.from_numpy(action) for input in inputs for action in input]
-        # flattened_action = torch.stack(flattened_action)
-        # print(flattened_action.shape)
-        # print(lengths)
-        return torch.from_numpy(np.array(inputs)), lengths
-    
+
+        return pad_sequence(inputs, batch_first=True, padding_value=torch.inf), lengths
+
+        # # TODO optimize this?
+        # max_length = max(lengths)
+        # for i in range(len(inputs)):
+        #     pad = torch.full((max_length - lengths[i], self.action_seq_length), torch.inf)
+        #     inputs[i] = torch.concat((inputs[i], pad))
+
+
+        # # pad with infinite actions
+        # inputs = [input + [torch.tensor([float('inf'), float('inf')]) for _ in range(max_length - len(input))] for input in inputs]
+
+        # # flattened_action = [action if isinstance(action, torch.Tensor) else torch.from_numpy(action) for input in inputs for action in input]
+        # # flattened_action = torch.stack(flattened_action)
+        # # print(flattened_action.shape)
+        # # print(lengths)
+        # return torch.from_numpy(np.array(inputs)), lengths
+
+__all__ = ["PaliGemmaWMActionProcessor"]
